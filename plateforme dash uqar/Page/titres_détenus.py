49cd61bff6d7a3c83a6a4f18d59df579c5adedf5
wisky107 <barrette.18@hotmail.com>
2023-04-03 13:43:26 -0400

    francis Mod

--- plateforme dash uqar/Page/titres_détenus.py
+++
@@ -1,8 +0,0 @@
-from components.titres_détenus import (
-    titres_détenus ,
-
-)
-
-
-def create_layout():
-    return titres_détenus 
--- plateforme dash uqar/components/titres_détenus.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd 
-# Lire les données depuis un fichier Excel
-fichier_excel = 'UQAR invest portfolio_2023-03-27.csv'  
-feuille = 'UQAR invested portfolio_2023-03'  
-
-# Charger les données dans un DataFrame
-df = pd.read_excel(fichier_excel, sheet_name=feuille)
