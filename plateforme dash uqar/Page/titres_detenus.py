78eb59d5fdc19ce937bd49b30cc87fbdbefe3eb8
wisky107 <barrette.18@hotmail.com>
2023-04-03 13:38:38 -0400

    francis mod

---
+++ plateforme dash uqar/Page/titres_detenus.py
@@ -0,0 +1,8 @@
+from components.titres_detenus import (
+    titres_detenus ,
+
+)
+
+
+def create_layout():
+    return titres_detenus 
---
+++ plateforme dash uqar/components/titres_detenus.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+
+# Charger les données dans un DataFrame
+df=pd.read_csv("assets/UQAR invested portfolio_2023-03-27.csv")
+
+tableau_titre = dbc.Col([
+    
+
+
+])
